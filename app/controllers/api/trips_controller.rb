46f58c5e122fa983e562654f391cac0e4552a1ad
Yiran Mao <yiranmao@gmail.com>
2014-04-29 23:53:38 -0400

    实现了基本的trips的api，增删改查

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -8,4 +8,41 @@ class Api::TripsController < ApplicationController
         #@user = @trip.owner
         #render json: @user
     end
+
+    def create
+        @trip = Trip.new(trip_params)
+        @trip.owner_id = current_user.id
+        if @trip.save
+          render json: {meta:{status: 200, msg:"OK"}}
+        else    
+          render json: {meta:{status: 404, msg:"create failed"}}
+        end
+    end
+
+    def show
+        render json: @trip = Trip.find(params[:id])
+    end
+
+    def update
+        @trip = Trip.find(params[:id])
+        if @trip.update(trip_params)
+          render json: {meta:{status: 200, msg:"OK"}}
+        else
+          render json: {meta:{status: 404, msg:"Update failed"}}
+        end
+    end
+
+    def destroy
+        @trip = Trip.find(params[:id])
+        if @trip.destroy
+          render json: {meta:{status: 200, msg:"OK"}}
+        else
+          render json: {meta:{status: 404, msg:"Delete failed"}}
+        end
+    end
+
+    private
+      def trip_params
+          params(:trip).permit(:destination, :start_date, :end_date, :fee)
+      end
 end
