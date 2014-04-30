ec906ed565db44498b86a064dac80c4c4ebd2a78
Yiran Mao <yiranmao@gmail.com>
2014-04-29 21:16:52 -0400

    登录成功，但是尚未保存和更新session的remember token

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,10 @@
 class Api::SessionsController < ApplicationController
   def create
-  end
-
-  private
-    def login_params
-      params.require(:user).permit(:email, :password)
+    user = User.find_by(email:params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+        render json: {meta:{status: 200, msg:"OK"}}
+    else
+        render json: {meta:{status: 404, msg:"login failed"}}
     end
+  end
 end
