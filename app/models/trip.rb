c71bb1836ef00d89b72c15f237aeac082bbcc462
Yiran Mao <yiranmao@gmail.com>
2014-05-09 17:07:30 -0400

    添加Trip和User相关的级联删除

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ActiveRecord::Base
-    has_many :trips_users
+    has_many :trips_users, dependent: :destroy
     has_many :users, through: :trips_users
-    has_many :chats
+    has_many :chats, dependent: :destroy
     belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
     before_save{ self.remember_token = User.hash(remember_token) }
     before_save{ self.email = email.downcase }
 
-    has_many :trips_users
+    has_many :trips_users, dependent: :destroy
     has_many :trips, through: :trips_users
-    has_many :trips_owned, :class_name => "Trip", :foreign_key => "owner_id"
-    has_many :chats
+    has_many :trips_owned, :class_name => "Trip", :foreign_key => "owner_id", dependent: :destroy
+    has_many :chats, dependent: :destroy
 
     has_secure_password
 
